0ca62bbecab9e634623efe15f001c78f360d3abf
K-S-A <atillas@ukr.net>
2017-03-25 05:29:23 +0200

    Update dist for deploy

--- test/spec/directives/logoutnav.js
+++ test/spec/directives/logoutnav.js
@@ -1,20 +1,20 @@
-'use strict';
-
-describe('Directive: logoutNav', function () {
-
-  // load the directive's module
-  beforeEach(module('ngTestApp'));
-
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
-
-  // it('should make hidden element visible', inject(function ($compile) {
-  //   element = angular.element('<logout-nav></logout-nav>');
-  //   element = $compile(element)(scope);
-  //   expect(element.text()).toBe('this is the logoutNav directive');
-  // }));
-});
+// 'use strict';
+//
+// describe('Directive: logoutNav', function () {
+//
+//   // load the directive's module
+//   beforeEach(module('ngTestApp'));
+//
+//   var element,
+//     scope;
+//
+//   beforeEach(inject(function ($rootScope) {
+//     scope = $rootScope.$new();
+//   }));
+//
+//   // it('should make hidden element visible', inject(function ($compile) {
+//   //   element = angular.element('<logout-nav></logout-nav>');
+//   //   element = $compile(element)(scope);
+//   //   expect(element.text()).toBe('this is the logoutNav directive');
+//   // }));
+// });
